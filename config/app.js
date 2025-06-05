853703c00c68eb25a5e7854fe99806fa7c69ade3
ccolorado-2023223 <ccolorado-2023223@kinal.edu.gt>
2025-06-05 15:51:22 -0600

    Empieza el backend del proyecto, se levanta el servidor

---
+++ config/app.js
@@ -0,0 +1,30 @@
+'use string'
+
+import express from "express"
+import morgan from "morgan"
+import helmet from "helmet"
+import cors from "cors"
+
+const configs = (app)=>{
+    app.use(express.json())
+    app.use(express.urlencoded({extended: false}))
+    app.use(cors())
+    app.use(helmet())
+    app.use(morgan('dev'))
+}
+
+const routes = (app)=>{
+
+}
+
+export const initServer =()=>{
+    const app = express()
+    try{
+        configs(app)
+        //routes(app)
+        app.listen(process.env.PORT)
+        console.log(`Server running in port ${process.env.PORT}`)
+    }catch(e){
+        console.error('S3erver init failed', e)
+    }
+}
