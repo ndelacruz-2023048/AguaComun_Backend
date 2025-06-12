c63752576451e0dea36f1d349a49508230fce7d5
jgonzalez-2023195 <jgonzalez-2023195@kinal.edu.gt>
2025-06-12 16:07:08 -0600

    Se arregla un error

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { initServer } from "./config/app.js";
 import { config } from "dotenv";
 import { connect } from "./config/mongo.js";
+import { initAdmin } from "./config/init.configs.js"
 
 config()
 connect()
