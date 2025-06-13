462a17b989032ac47ab6a51554957224c599259a
jgramajo-2023053 <jgramajo-2023053@kinal.edu.gt>
2025-06-13 17:06:48 -0600

    Creacion de archivos de Payment (model, controller, routes), Modelo de Payments definido

---
+++ src/Payment/payment.model.js
@@ -0,0 +1,43 @@
+import { Schema, model } from "mongoose"
+
+const paymentSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    amount: {
+      type: Number,
+      required: true
+    },
+    paymethod: {
+      type: String,
+      enum: ['Efectivo', 'Cheque'],
+      required: true
+    },
+    bankcheck: {
+      type: Number,
+      required: function () {
+        return this.paymethod === 'Cheque'
+      }
+    },
+    address: {
+      type: String,
+      required: true
+    },
+    manualDate: {
+      type: Date,
+      required: false
+    },
+    status: {
+      type: String,
+      enum: ['Pendiente', 'Confirmado'],
+      default: 'Pendiente'
+    }
+  },
+  {
+    timestamps: true
+  }
+)
+
+export default model('Payment', paymentSchema)
