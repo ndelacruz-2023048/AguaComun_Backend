28abdf949423b5bd1d93dfaa92280737181f3329
jgonzalez-2023195 <jgonzalez-2023195@kinal.edu.gt>
2025-06-19 16:09:26 -0600

    Se arregla una letra

--- src/CommunityManager/community.routes.js
+++ src/CommunityManager/community.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import {getCommunities,
-        } from "./Community.controller.js";
+        } from "./community.controller.js";
 const communityRoutesManager= Router();
 
 communityRoutesManager.get("/view", getCommunities);
